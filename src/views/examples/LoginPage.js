f52e4f6544b3f090979898e9c9b9023bb5ab3981
PawelJadach <pawel.jadach.96@gmail.com>
2020-04-04 11:09:11 +0200

    Init project

---
+++ src/views/examples/LoginPage.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Col
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import TransparentFooter from "components/Footers/TransparentFooter.js";
+
+function LoginPage() {
+  const [firstFocus, setFirstFocus] = React.useState(false);
+  const [lastFocus, setLastFocus] = React.useState(false);
+  React.useEffect(() => {
+    document.body.classList.add("login-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("login-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  });
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="page-header clear-filter" filter-color="blue">
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage: "url(" + require("assets/img/login.jpg") + ")"
+          }}
+        ></div>
+        <div className="content">
+          
+        </div>
+        <TransparentFooter />
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;
